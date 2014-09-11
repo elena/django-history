d0f9cbdf9e884dbd482601df13df8d003da2763f
Elena Williams <elena.williams@thecut.net.au>
2014-09-11 16:16:17 +0800

    Add translation strings and additional help_text to Event in events models.

--- events/models.py
+++ events/models.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 
 class Series(models.Model):
-    name = models.CharField(max_length=64)
-    website = models.URLField()
+    name = models.CharField(_('Event name'),
+                            max_length=64,
+                            help_text="eg. DjangoCon Australia")
+    website = models.URLField(_('Website'))
 
 
 class Event(models.Model):
     series = models.ForeignKey('events.Series', null=True, blank=True)
-    series_number = models.CharField(max_length=32, blank=True,
+    series_number = models.CharField(_('Series number'),
+                                     max_length=32,
+                                     blank=True,
                                      help_text='Example: Year or yyyy-mm-dd')
 
-    name = models.CharField(max_length=64)
-    website = models.URLField()
-    date_start = models.DateTimeField()
-    date_end = models.DateTimeField()
+    name = models.CharField(_('Event name'),
+                            max_length=64,
+                            help_text="")
+    website = models.URLField(_('Website'))
+    date_start = models.DateTimeField(_('Start date/time'))
+    date_end = models.DateTimeField(_('End date/time'))
 
     def __str__(self):
         return self.name
