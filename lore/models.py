750311560d42c3648ca2b2301de4c853e099eef1
Elena Williams <elena+seryy@elena.net.au>
2014-09-11 22:47:23 +0800

    Add translations on Talk in lore models.

--- lore/models.py
+++ lore/models.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from taggit.managers import TaggableManager
+from django.utils.translation import ugettext_lazy as _
 
 
 class Talk(models.Model):
     event = models.ForeignKey('events.Event', null=True, blank=True)
-    tags = TaggableManager()
+    tags = TaggableManager(_('Tags'))
