7d1335f7743d65f7c94c60f5a29cf2eb70a7417e
Elena Williams <elena+seryy@elena.net.au>
2014-09-13 19:33:04 +0800

    Add initial admin for events EventInline, Series with inlines and Event.

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Series, Event
+
+
+class SeriesAdmin(admin.ModelAdmin):
+    model = Series
+
+admin.site.register(Series, SeriesAdmin)
+
+
+class EventAdmin(admin.TabularInline):
+    model = Event
+
+admin.site.register(Event, EventAdmin)
