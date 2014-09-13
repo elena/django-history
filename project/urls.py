0e8f2e51819374038f8272f00ca4d1f8c9f64976
Elena Williams <elena+seryy@elena.net.au>
2014-09-13 22:00:12 +0800

    Add events urls to project urls. Remove defaults.

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, include, url
+# -*- coding: utf-8 -*-
+from django.conf.urls import include, patterns
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+urls = patterns('',
+
+    # Events
+    (r'^events/', include('events.urls')),
+
 )
