8ab5b60423aae009e24d51f1efc8d408f6c30429
Elena Williams <elena+seryy@elena.net.au>
2014-09-13 19:33:37 +0800

    Add EventInline and inlines to SeriesAdmin in events admin.

--- events/admin.py
+++ events/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from .models import Series, Event
 
 
+class EventInlines(admin.TabularInline):
+    model = Event
+
+
 class SeriesAdmin(admin.ModelAdmin):
     model = Series
+    inlines = [EventInlines]
 
 admin.site.register(Series, SeriesAdmin)
 
