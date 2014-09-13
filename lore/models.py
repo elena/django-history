7668cc766a4dbecf60659eeedaddc03e7fa43b2e
Elena Williams <elena+seryy@elena.net.au>
2014-09-13 23:39:06 +0800

    Add Category class to lore models.

--- lore/models.py
+++ lore/models.py
@@ -4,6 +4,13 @@ from taggit.managers import TaggableManager
 from django.utils.translation import ugettext_lazy as _
 
 
+class Category(models.Model):
+    name = models.CharField(_('Title'), max_length=256, null=True, blank=True)
+    colour = models.CharField(_('Color'), max_length=7, null=True, blank=True,
+                              help_text = "Arbitrary colour for presentation")
+
+    def __str__(self):
+        return self.name
 
 
 class Talk(models.Model):
