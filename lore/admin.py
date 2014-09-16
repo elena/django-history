e701ecb0c0516e629733ccc3ca2e019a9d07d236
Elena Williams <elena.williams@thecut.net.au>
2014-09-16 12:55:05 +0800

    Add basic admin for Talk, Speaker and Category in lore admin.

--- lore/admin.py
+++ lore/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Talk, Speaker
+
+
+class TalkAdmin(admin.ModelAdmin):
+    model = Talk
+    prepopulated_fields = {"slug":("title",)}
+
+admin.site.register(Talk, TalkAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    prepopulated_fields = {"slug":("name",)}
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class SpeakerAdmin(admin.ModelAdmin):
+    model = Speaker
+    prepopulated_fields = {"slug":("pk", "full_name",)}
+
+admin.site.register(Speaker, SpeakerAdmin)
