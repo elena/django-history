116a46892db5b9bd01dfc688e022e03aefbd6334
Elena Williams <elena+seryy@elena.net.au>
2014-09-29 20:10:49 +0800

    Add ``SpeakerAdmin`` init and display_* methods for pyvideo, people, photo and finding.

--- lore/admin.py
+++ lore/admin.py
@@ -20,4 +20,40 @@ class SpeakerAdmin(admin.ModelAdmin):
     model = Speaker
     prepopulated_fields = {"slug":("full_name",)}
 
+    def __init__(self, *args, **kwargs):
+        super(SpeakerAdmin, self).__init__(*args, **kwargs)
+        self.list_display = ['full_name', self.display_pyvideo,
+                             self.display_people, self.display_finding,
+                             self.display_photo]
+
+    def display_pyvideo(self, obj):
+        if obj.pyvideo_pk:
+            return '<a href="{0}" target="_blank">{1}</a>'.format(
+                obj.get_pyvideo_url(), obj.pyvideo_pk)
+        else: return ''
+    display_pyvideo.allow_tags = True
+    display_pyvideo.short_description = 'PyVideo pk'
+
+    def display_people(self, obj):
+        if obj.people:
+            return '<a href="{0}" target="_blank">{1}</a>'.format(
+                obj.get_people_url(), obj.people)
+        else: return ''
+    display_people.allow_tags = True
+    display_people.short_description = 'DjangoPeople'
+
+    def display_finding(self, obj):
+        if obj.people:
+            return obj.people_finding
+        else: return ''
+    display_finding.allow_tags = True
+    display_finding.short_description = 'Details'
+
+    def display_photo(self, obj):
+        if obj.people_photo:
+            return '<img src="{0}" width=50>'.format(obj.people_photo)
+    display_photo.allow_tags = True
+    display_photo.short_description = 'Photo'
+
+
 admin.site.register(Speaker, SpeakerAdmin)
