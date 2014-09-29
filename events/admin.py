0225adf998bd25db7bc161dc6c22a3be69858cf3
Elena Williams <elena+seryy@elena.net.au>
2014-09-29 20:06:29 +0800

    Add ``EventAdmin`` list_display and list_editable in events.

--- events/admin.py
+++ events/admin.py
@@ -17,6 +17,10 @@ admin.site.register(Series, SeriesAdmin)
 
 class EventAdmin(admin.ModelAdmin):
     model = Event
+    list_display = ['name', 'colour', 'pyvideo_category_pk',
+                    'pyvideo_category_title',
+                    'youtube_channel_id', 'google_plus', 'website']
+    list_editable = ['pyvideo_category_title', 'youtube_channel_id']
     prepopulated_fields = {"slug":("name",)}
 
 admin.site.register(Event, EventAdmin)
